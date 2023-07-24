f0aa85587272147cc4c2d2fe0c5c2735d8f30e17
Konstantin <konvod93@gmail.com>
2023-07-24 15:14:04 +0300

    deleted key in Col

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -5,7 +5,7 @@ import CardSinglePost from "./cardsinglepost";
 const PostList = ({ posts }) => {
   return posts.map(post => {
     return (
-      <Col lg={4} xs={12} sm={6} key={post.id}>
+      <Col lg={4} xs={12} sm={6}>
         <CardSinglePost key={post.fields.slug} {...post} />
       </Col>
     )
