d3b5b26121335cff96ab2579d3bd6fc8b2463868
Konstantin <konvod93@gmail.com>
2023-07-24 15:19:37 +0300

    deleted unusing imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import { graphql, Script } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import { Container, Row, Col, Card } from "react-bootstrap"
+import { Container, Row } from "react-bootstrap"
 import PostList from "../components/postlist"
 
 
