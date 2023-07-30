b15c25ea2542ca8c6d8c25d8df7dcdee144c02d6
Konstantin <konvod93@gmail.com>
2023-07-30 15:20:00 +0300

    fixing errors - added unique keys in PostList and TagList

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -6,7 +6,7 @@ import CardOfPost from './CardOfPost'
 const PostsList = ({ posts }) => {
   return posts?.map(post => {
     return (
-      <Col lg={4} xs={12} sm={6}>
+      <Col lg={4} xs={12} sm={6} key={post.id}>
         <CardOfPost key={post.fields.slug} {...post} />
       </Col>
     )
--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -7,7 +7,7 @@ import { Button } from "react-bootstrap";
 const TagsList = ({ tags }) => {
   return tags?.map(tag => {
     return (
-        <Link to={`/tags/${tag}`}><Button variant="outline-dark" className="m-1">{ tag }</Button></Link>
+        <Link to={`/tags/${tag}`} key={tag}><Button variant="outline-dark" className="m-1">{ tag }</Button></Link>
       )
   })
 }
