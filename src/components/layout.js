12210c4b2bdfc77f3f194faa09d586ab0eb8dcd9
Konstantin <konvod93@gmail.com>
2023-08-12 16:09:34 +0300

    fixing footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,41 +36,73 @@ const Layout = ({ children }) => {
       <main>{children}</main>
       <footer
         style={{
-          display: `flex`,
           boxSizing: `border-box`,
           width: `100%`,
           marginTop: `var(--space-5)`,
           fontSize: `var(--font-sm)`,
           backgroundColor: `black`,
-          color: `white`,          
+          color: `white`,
           opacity: `0.9`,
+          marginTop: `80px`,
           paddingTop: `10px`,
           paddingBottom: `10px`,
-          justifyContent: `space-between`,
-          paddingLeft: `50px`,
-          paddingRight: `50px`
         }}
       >
-        <span>
-        © {new Date().getFullYear()} &middot; Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>       
-        {` `}
-        </span>
-        <span>
-        Background image from
-        {` `}
-        <a href="https://ru.freepik.com/free-vector/white-abstract-background_11852424.htm#query=%D1%84%D0%BE%D0%BD%D0%B4%D0%BB%D1%8F%D1%81%D0%B0%D0%B9%D1%82%D0%B0&position=5&from_view=keyword&track=ais#position=5&query=%D1%84%D0%BE%D0%BD%D0%B4%D0%BB%D1%8F%D1%81%D0%B0%D0%B9%D1%82%D0%B0?log-in=google">Freepik</a>
-        </span>
-        <SocialIcon url="#" network="facebook"/>
-        <SocialIcon url="#" network="telegram"/>
-        <SocialIcon url="#" network="instagram"/>
-        <SocialIcon url="#" network="twitter"/>
-        <SocialIcon url="#" network="linkedin"/>
-        <SocialIcon url="#" network="pinterest"/>
-        <SocialIcon url="#" network="github"/>
-        <SocialIcon url="#" network="gitlab"/>
-        <SocialIcon url="#" network="youtube"/>
+        <div
+          style={{
+            display: `flex`,
+            justifyContent: `center`,
+          }}
+        >
+          <div>
+            <SocialIcon url="#" network="facebook" style={{ height: 20, width: 20, margin: 5}}/>
+          </div>
+          <div>
+            <SocialIcon url="#" network="telegram" style={{ height: 20, width: 20, margin: 5}}/>
+          </div>
+          <div>
+            <SocialIcon url="#" network="instagram" style={{ height: 20, width: 20, margin: 5}}/>
+          </div>
+          <div>
+            <SocialIcon url="#" network="twitter" style={{ height: 20, width: 20, margin: 5}}/>
+          </div>
+          <div>
+            <SocialIcon url="#" network="linkedin" style={{ height: 20, width: 20, margin: 5}}/>
+          </div>
+          <div>
+            <SocialIcon url="#" network="pinterest" style={{ height: 20, width: 20, margin: 5}}/>
+          </div>
+          <div>
+            <SocialIcon url="#" network="github" style={{ height: 20, width: 20, margin: 5}}/>
+          </div>
+          <div>
+            <SocialIcon url="#" network="gitlab" style={{ height: 20, width: 20, margin: 5}}/>
+          </div>
+          <div>
+            <SocialIcon url="#" network="youtube" style={{ height: 20, width: 20, margin: 5}}/>
+          </div>
+        </div>
+        <div
+          style={{  
+            marginTop: `10px`,          
+            marginLeft: `10px`,
+            marginRight: `10px`,            
+          }}
+        >
+          <div style={{ textAlign: `center`}}>
+            © {new Date().getFullYear()} &middot; Built with
+            {` `}
+            <a href="https://www.gatsbyjs.com">Gatsby</a>
+            {` `}
+          </div>
+          <div style={{ textAlign: `end`, fontSize: `x-small`, marginTop: `10px`}}>
+            Background image from
+            {` `}
+            <a href="https://ru.freepik.com/free-vector/white-abstract-background_11852424.htm#query=%D1%84%D0%BE%D0%BD%D0%B4%D0%BB%D1%8F%D1%81%D0%B0%D0%B9%D1%82%D0%B0&position=5&from_view=keyword&track=ais#position=5&query=%D1%84%D0%BE%D0%BD%D0%B4%D0%BB%D1%8F%D1%81%D0%B0%D0%B9%D1%82%D0%B0?log-in=google">
+              Freepik
+            </a>
+          </div>
+        </div>
       </footer>
     </>
   )
